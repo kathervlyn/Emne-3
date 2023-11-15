bf11c7d812bc91e24a4976c9761a7e46c1258a11
kathervlyn <a.h.arnesen@hotmail.com>
2023-11-15 13:03:08 +0100

    Random i C# Ferdig

--- 04 - Random i C#/Random i C#/Random i C#/Program.cs
+++ 04 - Random i C#/Random i C#/Random i C#/Program.cs
@@ -4,21 +4,34 @@
     {
         static void Main(string[] args)
         {
+            GiveHobby();
+        }
+
+        static void GiveHobby()
+        {
+            Console.WriteLine("Who gets a new hobby? \n");
+            string Name = Console.ReadLine();
+            Console.WriteLine();
             Random rand = new Random();
-            var randomNumber = rand.Next(0, 3);
+            var randomNumber = rand.Next(0, 4);
             if (randomNumber == 0)
             {
-                Console.WriteLine("Tallet Ble 0");
+                Console.WriteLine($"{Name} is now a Magician");
             }
             else if (randomNumber == 1)
             {
-                Console.WriteLine("Tallet ble 1");
+                Console.WriteLine($"{Name} is now a Climber");
+            }
+            else if (randomNumber == 2)
+            {
+                Console.WriteLine($"{Name} is now a Carpenter");
             }
             else
             {
-                Console.WriteLine("Tallet Ble 2");
+                Console.WriteLine($"{Name} is now a Musician");
             }
+            Console.WriteLine();
+            GiveHobby();
         }
-
     }
 }

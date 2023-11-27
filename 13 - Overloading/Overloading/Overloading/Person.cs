916c66fb8c7ff9989404a9a565be149c84da899a
kathervlyn <a.h.arnesen@hotmail.com>
2023-11-27 14:58:38 +0100

    Default values Oppgaven

---
+++ 13 - Overloading/Overloading/Overloading/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading
+{
+    internal class Person
+    {
+        public string Name;
+        public int Age;
+        public string Address;
+
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Person(string name, int age, string adress)
+        {
+            Name = name;
+            Age = age;
+            Address = adress;
+        }
+
+        public void PrintWelcomeMessage(string velkommenMelding)
+        {
+            Console.WriteLine(velkommenMelding + Name);
+        }
+
+    }
+}
---
+++ 13 - Overloading/Overloading/Overloading/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Overloading
+{
+    internal class Program
+    {
+         static void Main()
+        {
+            var person = new Person("Kath", 23,"Gressvik" );
+            person.PrintWelcomeMessage("Hei og velkommen ");
+        }
+    }
+}
